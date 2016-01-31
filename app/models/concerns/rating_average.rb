e870f2e2ee8e42e984d0380476f8b89bd168bf85
Satu Nenonen <shnenonen@gmail.com>
2016-01-31 21:20:12 +0200

    korjauksia average ratingiin

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,6 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
+    return 0 if ratings.empty?
     total = self.ratings.inject(0) { |sum, n| sum + n.score }
     return total / self.ratings.count.to_f
   end
