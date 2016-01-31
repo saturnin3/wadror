49159699b5202830596239ea548b4494f6aa6aa2
Satu Nenonen <shnenonen@gmail.com>
2016-01-31 21:06:51 +0200

    Korjauksia average ratingiin

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,18 +1,20 @@
 class Beer < ActiveRecord::Base
+ include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def average_rating
+#  def average_rating
 #    total = 0
  #   self.ratings.each do |r|
   #   total += r.score
    # end
    # return total / self.ratings.count
   #  self.ratings.average(:score)
-    total = self.ratings.inject(0){ |sum, n| sum + n.score }
+ #   total = self.ratings.inject(0){ |sum, n| sum + n.score }
    # total = self.ratings.map{ |r| r.score}.inject{ |sum, n| sum + n}
-    return total / self.ratings.count
-  end 
+#    return total / self.ratings.count
+#  end 
   def to_s
     i = self.brewery_id
     p = Brewery.find i
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -3,7 +3,7 @@ module RatingAverage
 
   def average_rating
     total = self.ratings.inject(0) { |sum, n| sum + n.score }
-    return total / self.ratings.count
+    return total / self.ratings.count.to_f
   end
 
 end
