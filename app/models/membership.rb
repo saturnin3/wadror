adf41a6809cebe96ca66dddf9cda72afb77e71ab
Satu Nenonen <shnenonen@gmail.com>
2016-02-02 23:18:16 +0200

    Viikko 3 tehtävä 7-8

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,8 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :beer_club
 
+  #validates :user_id, uniqueness: { scope: :beer_club_id, message: "Membership exists already." }
+  #validates :beer_club_id, uniqueness: { scope: :user_id, message: "Membership exists already."}
+  #validates :user_id, presence: true
+
 end
